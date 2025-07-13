bd5bb21e1a2703f1ed5414f24b5ca8830603bf6d
Akash <hafejmohammadakash@gmail.com>
2025-07-13 12:20:00 +0600

    server site setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json()); 
+
+
+
+
+
+
+
+// Root Route
+app.get('/', (req, res) => {
+  res.send('Hello from Express server!');
+});
+
+// Start Server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
